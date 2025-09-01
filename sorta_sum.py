c2e2cf5bca48d8f67b8ca8f06192aeab58cc0184
Suchitra Mummina <suchitramummina1@gmail.com>
2025-09-01 18:30:01 +0530

    sorta_sum.py

---
+++ sorta_sum.py
@@ -0,0 +1,13 @@
+"""
+problem statement:
+Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.
+sorta_sum(3, 4) → 7
+sorta_sum(9, 4) → 20
+sorta_sum(10, 11) → 21
+"""
+#code
+def sorta_sum(a, b):
+    total = a + b
+    if 10 <= total <= 19:
+        return 20
+    return total
